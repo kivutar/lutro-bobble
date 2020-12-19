8cec2c14b1f1b82fbb0307779a65aabcefe0d12f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2020-12-19 21:58:08 +0700

    Auto next stage

--- inter.lua
+++ inter.lua
@@ -3,22 +3,12 @@ inter.__index = inter
 
 function newInter(n)
 	n.type = "inter"
-	n.t = 0
+	n.t = 60
 	n.PRESSED = 0
 	return setmetatable(n, inter)
 end
 
 function inter:update(dt)
-	local JOY_START  = love.joystick.isDown(1, RETRO_DEVICE_ID_JOYPAD_START)
-	if JOY_START then
-		self.PRESSED = self.PRESSED + 1
-	end
-
-	if self.PRESSED == 1 then
-		love.audio.play(SFX_ok)
-		self.t = 60
-	end
-
 	if self.t > 0 then
 		self.t = self.t - 1
 		if self.t == 1 then
